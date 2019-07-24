5b87bf13c2ee6e8b372a3dd16dd96cdade3f157d
Ayan Banerjee <ayanb1999@gmail.com>
2019-07-24 21:50:11 +0530

    Adding contribute viewContainer

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,7 @@
-
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-
+	context.subscriptions.push(vscode.commands.registerCommand('remixPlugin.activate', () => {}));
 }
 
 // this method is called when your extension is deactivated
